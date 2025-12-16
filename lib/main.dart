1de65872d5ed88449d341f64e74dc7616851074b
AlexisTci <ing15@tciconsultoria.com>
2025-12-16 10:26:46 -0600

    se agrego splash nativo y se quito splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:provider/provider.dart';
 import 'package:time_gate/pages/pages.dart';
-import 'package:time_gate/pages/spash_page.dart';
+// import 'package:time_gate/pages/spash_page.dart';
 import 'package:time_gate/providers/tabbar_provider.dart'; 
 import 'package:time_gate/themes/app_theme.dart';
 import 'package:intl/date_symbol_data_local.dart';  
@@ -9,37 +10,99 @@ import './providers/auth_provider.dart';
 
 
 // void main() => runApp(const MyApp());
-void main() {
-  initializeDateFormatting().then((_) => runApp(const MyApp()));
+// void main() {
+//   initializeDateFormatting().then((_) => runApp(const MyApp()));
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+   
+//     return MultiProvider(
+//       providers: [
+//         ChangeNotifierProvider(create: (_) => TabbarProvider()),
+//         ChangeNotifierProvider(create: (_) => AuthProvider()),
+//       ],
+//       child: MaterialApp(
+//         title: 'Material App',
+//         debugShowCheckedModeBanner: false,
+//         initialRoute: 'splash',
+//         routes: {
+//           'splash': (_) => const SplashPage(),
+//           'main': (_) => const TabsPage(),
+//           'login': (_) => const LoginPage(),
+//           'vacation request': (_) => const VacationRequestPage(),
+//           'permit application': (_) => const PermitApplicationPage(),
+//         },
+//         onUnknownRoute: (settings) {
+//           return MaterialPageRoute(
+//             builder: (context) => const TabsPage(),
+//           );
+//         },
+//         theme: AppTheme.lightTheme,
+//       ),
+//     );
+//   }
+// }
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  await initializeDateFormatting();
+
+  final authProvider = AuthProvider();
+  
+  // 1. Cargamos la sesión. Aseguramos que sessionExists sea bool, nunca null.
+  bool sessionExists = false;
+  try {
+    sessionExists = await authProvider.loadSession();
+  } catch (e) {
+    sessionExists = false;
+  }
+
+  FlutterNativeSplash.remove();
+
+  // 2. Pasamos la instancia y la ruta decidida
+  runApp(MyApp(
+    initialRoute: sessionExists ? 'main' : 'login',
+    authProvider: authProvider,
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final String initialRoute;
+  final AuthProvider authProvider;
+
+  // 3. El constructor requiere ambos parámetros
+  const MyApp({
+    super.key, 
+    required this.initialRoute, 
+    required this.authProvider
+  });
 
   @override
   Widget build(BuildContext context) {
-   
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => TabbarProvider()),
-        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        // 4. USAMOS .value y nos aseguramos de pasarle el objeto recibido
+        ChangeNotifierProvider<AuthProvider>.value(value: authProvider),
       ],
       child: MaterialApp(
         title: 'Material App',
         debugShowCheckedModeBanner: false,
-        initialRoute: 'splash',
+        initialRoute: initialRoute,
         routes: {
-          'splash': (_) => const SplashPage(),
           'main': (_) => const TabsPage(),
           'login': (_) => const LoginPage(),
           'vacation request': (_) => const VacationRequestPage(),
           'permit application': (_) => const PermitApplicationPage(),
         },
-        onUnknownRoute: (settings) {
-          return MaterialPageRoute(
-            builder: (context) => const TabsPage(),
-          );
-        },
+        onUnknownRoute: (settings) => MaterialPageRoute(
+          builder: (context) => const TabsPage(),
+        ),
         theme: AppTheme.lightTheme,
       ),
     );
