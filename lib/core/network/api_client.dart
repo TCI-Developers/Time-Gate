79c52a4a1bde66d810068e0849de98f66ca4aa38
AlexisTci <ing15@tciconsultoria.com>
2026-02-04 11:05:34 -0600

    icono de app agregado

--- lib/core/network/api_client.dart
+++ lib/core/network/api_client.dart
@@ -15,7 +15,7 @@ class ApiClient {
   ApiClient._internal() {
     _dio = Dio(
       BaseOptions(
-        baseUrl: 'https://9d9ddd081eee.ngrok-free.app/api',
+        baseUrl: 'https://f8bf-187-142-124-197.ngrok-free.app/api',
         connectTimeout: const Duration(seconds: 10),
         receiveTimeout: const Duration(seconds: 10),
         headers: {
