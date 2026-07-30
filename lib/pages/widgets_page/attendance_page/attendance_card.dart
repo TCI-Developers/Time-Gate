293a080e33037d4e1db8566197745ec48f62da38
TCI-Developers <45832812+TCI-Developers@users.noreply.github.com>
2026-07-30 15:12:50 -0600

    Fix: Solucionado error de Infinity o NaN en los indicadores de asistencia

--- lib/pages/widgets_page/attendance_page/attendance_card.dart
+++ lib/pages/widgets_page/attendance_page/attendance_card.dart
@@ -3,90 +3,107 @@ import 'package:time_gate/themes/custom_styles.dart';
 import 'package:time_gate/utils/responsive_utils.dart';
 
 class AttendanceCard extends StatelessWidget {
-
   final Color color;
   final String type;
   final double maxProgress;
-  final double currentProgress; 
-  
+  final double currentProgress;
+
   const AttendanceCard({
-    super.key, required this.color, required this.type, required this.maxProgress, required this.currentProgress,
+    super.key,
+    required this.color,
+    required this.type,
+    required this.maxProgress,
+    required this.currentProgress,
   });
-  
+
   @override
   Widget build(BuildContext context) {
     final textOsw14bold400Na = Theme.of(context).textTheme.textOsw14bold400Na;
-    final textOsw12bold400Secondary = Theme.of(context).textTheme.textOsw12bold400Secondary;
-    final textOsw11bold400Secondary = Theme.of(context).textTheme.textOsw11bold400Secondary;
+    final textOsw12bold400Secondary = Theme.of(
+      context,
+    ).textTheme.textOsw12bold400Secondary;
+    final textOsw11bold400Secondary = Theme.of(
+      context,
+    ).textTheme.textOsw11bold400Secondary;
 
     final fontSizedGrow = getResponsiveScaleFactor(context);
-   
-    double progressRatio = currentProgress / maxProgress;
+
+    double progressRatio = (maxProgress > 0)
+        ? (currentProgress / maxProgress)
+        : 0.0;
 
     const double circleSize = 50.0;
-    const double borderWidth = 3.0; 
+    const double borderWidth = 3.0;
 
     return Container(
       padding: const EdgeInsets.all(10),
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(10),
-        boxShadow: const[
-          BoxShadow(
-            color: Colors.black26,
-            blurRadius: 4,
-            offset: Offset(0, 4),
-          ),
+        boxShadow: const [
+          BoxShadow(color: Colors.black26, blurRadius: 4, offset: Offset(0, 4)),
         ],
       ),
       child: Row(
         children: [
-         
           SizedBox(
-            width: circleSize*fontSizedGrow,
-            height: circleSize*fontSizedGrow,
+            width: circleSize * fontSizedGrow,
+            height: circleSize * fontSizedGrow,
             child: Stack(
               alignment: Alignment.center,
               children: [
-                
                 SizedBox(
-                  width: circleSize*fontSizedGrow,
-                  height: circleSize*fontSizedGrow,
+                  width: circleSize * fontSizedGrow,
+                  height: circleSize * fontSizedGrow,
                   child: CircularProgressIndicator(
-                    value: progressRatio,                    
-                    strokeWidth: borderWidth,                   
-                    backgroundColor: Colors.grey[300], 
+                    value: progressRatio,
+                    strokeWidth: borderWidth,
+                    backgroundColor: Colors.grey[300],
                     valueColor: AlwaysStoppedAnimation<Color>(color),
                   ),
                 ),
-                
+
                 Column(
-                  mainAxisSize: MainAxisSize.min, 
+                  mainAxisSize: MainAxisSize.min,
                   children: [
                     Text(
-                      'Quedan', 
-                      style: textOsw11bold400Secondary.copyWith(fontSize: 11*fontSizedGrow)
+                      'Quedan',
+                      style: textOsw11bold400Secondary.copyWith(
+                        fontSize: 11 * fontSizedGrow,
+                      ),
                     ),
                     Text(
-                      '${ maxProgress.toInt() - currentProgress.toInt()}', 
-                      style: textOsw12bold400Secondary.copyWith(fontSize: 12*fontSizedGrow)
-                    )
+                      '${maxProgress.toInt() - currentProgress.toInt()}',
+                      style: textOsw12bold400Secondary.copyWith(
+                        fontSize: 12 * fontSizedGrow,
+                      ),
+                    ),
                   ],
                 ),
               ],
             ),
           ),
-       
-    
-          const SizedBox(width: 10), 
-          
+
+          const SizedBox(width: 10),
+
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(type, style: textOsw14bold400Na.copyWith(color: color, fontSize: 14*fontSizedGrow)),
-              Text('${currentProgress.toInt()} de ${maxProgress.toInt()} Disponibles', style: textOsw11bold400Secondary.copyWith(fontSize: 11*fontSizedGrow),)
+              Text(
+                type,
+                style: textOsw14bold400Na.copyWith(
+                  color: color,
+                  fontSize: 14 * fontSizedGrow,
+                ),
+              ),
+              Text(
+                '${currentProgress.toInt()} de ${maxProgress.toInt()} Disponibles',
+                style: textOsw11bold400Secondary.copyWith(
+                  fontSize: 11 * fontSizedGrow,
+                ),
+              ),
             ],
-          )
+          ),
         ],
       ),
     );
--- lib/pages/widgets_page/attendance_page/monthly_registration_card.dart
+++ lib/pages/widgets_page/attendance_page/monthly_registration_card.dart
@@ -12,86 +12,110 @@ class MonthlyRegistrationCard extends StatelessWidget {
   final double asistenciasMensuales;
 
   const MonthlyRegistrationCard({
-    super.key, required this.asistencia, 
-    required this.vacaciones, 
-    required this.ausencias, 
-    required this.ausenciasPermitidas, 
-    required this.totalVacaciones, 
+    super.key,
+    required this.asistencia,
+    required this.vacaciones,
+    required this.ausencias,
+    required this.ausenciasPermitidas,
+    required this.totalVacaciones,
     required this.asistenciasMensuales,
   });
 
   @override
   Widget build(BuildContext context) {
+    final titleOsw20Bold500Secondary = Theme.of(
+      context,
+    ).textTheme.titleOsw20Bold500Secondary;
 
-    final titleOsw20Bold500Secondary = Theme.of(context).textTheme.titleOsw20Bold500Secondary;
-    
     final fontSizedGrow = getResponsiveScaleFactor(context);
 
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
       decoration: BoxDecoration(
-        boxShadow: const[
-          BoxShadow(
-            color: Colors.black26,
-            blurRadius: 4,
-            offset: Offset(0, 4),
-    
-          ),
+        boxShadow: const [
+          BoxShadow(color: Colors.black26, blurRadius: 4, offset: Offset(0, 4)),
         ],
         color: Colors.white,
-        borderRadius: BorderRadius.circular(10)
+        borderRadius: BorderRadius.circular(10),
       ),
       child: Column(
-    
         children: [
-          Text('Registro Mensual', style: titleOsw20Bold500Secondary.copyWith(fontSize: 20*fontSizedGrow),),
-          const SizedBox(height: 10,),
+          Text(
+            'Registro Mensual',
+            style: titleOsw20Bold500Secondary.copyWith(
+              fontSize: 20 * fontSizedGrow,
+            ),
+          ),
+          const SizedBox(height: 10),
           Stack(
             alignment: AlignmentGeometry.center,
             children: [
-               SizedBox(
-                  width: 40*fontSizedGrow,
-                  height: 40*fontSizedGrow,
-                  child: CircularProgressIndicator(                 
-                    value: (ausenciasPermitidas > 0) ? (1.0 - (ausencias / ausenciasPermitidas)) : 1.0,                 
-                    strokeWidth: 4,                   
-                    backgroundColor: const Color.fromARGB(255, 187, 51, 41), 
-                    valueColor: AlwaysStoppedAnimation<Color>(const Color.fromARGB(255, 255, 193, 193)),
-                  ),
-              ),
               SizedBox(
-                  width: 70*fontSizedGrow,
-                  height: 70*fontSizedGrow,
-                  child: CircularProgressIndicator(                
-                    value: (ausenciasPermitidas > 0) ? (1-(vacaciones / totalVacaciones)) : 1.0,                    
-                    strokeWidth: 4,                   
-                    backgroundColor: Colors.yellow, 
-                    valueColor: AlwaysStoppedAnimation<Color>(const Color.fromARGB(255, 255, 193, 193)),
+                width: 40 * fontSizedGrow,
+                height: 40 * fontSizedGrow,
+                child: CircularProgressIndicator(
+                  value: (ausenciasPermitidas > 0)
+                      ? (1.0 - (ausencias / ausenciasPermitidas))
+                      : 1.0,
+                  strokeWidth: 4,
+                  backgroundColor: const Color.fromARGB(255, 187, 51, 41),
+                  valueColor: AlwaysStoppedAnimation<Color>(
+                    const Color.fromARGB(255, 255, 193, 193),
                   ),
                 ),
+              ),
               SizedBox(
-                  width: 105*fontSizedGrow,
-                  height: 105*fontSizedGrow,
-                  child: CircularProgressIndicator(
-                    // value: (1.0 - (asistencia / asistenciasMensuales)),                    
-                    value: (asistenciasMensuales > 0) ? (1.0 - (asistencia / asistenciasMensuales)) : 1.0,                    
-                    strokeWidth: 4,                   
-                    backgroundColor: const Color.fromARGB(255, 54, 125, 56), 
-                    valueColor: AlwaysStoppedAnimation<Color>(const Color.fromARGB(255, 255, 193, 193)),
+                width: 70 * fontSizedGrow,
+                height: 70 * fontSizedGrow,
+                child: CircularProgressIndicator(
+                  value: (totalVacaciones > 0)
+                      ? (1 - (vacaciones / totalVacaciones))
+                      : 1.0,
+                  strokeWidth: 4,
+                  backgroundColor: Colors.yellow,
+                  valueColor: AlwaysStoppedAnimation<Color>(
+                    const Color.fromARGB(255, 255, 193, 193),
                   ),
+                ),
+              ),
+              SizedBox(
+                width: 105 * fontSizedGrow,
+                height: 105 * fontSizedGrow,
+                child: CircularProgressIndicator(
+                  // value: (1.0 - (asistencia / asistenciasMensuales)),
+                  value: (asistenciasMensuales > 0)
+                      ? (1.0 - (asistencia / asistenciasMensuales))
+                      : 1.0,
+                  strokeWidth: 4,
+                  backgroundColor: const Color.fromARGB(255, 54, 125, 56),
+                  valueColor: AlwaysStoppedAnimation<Color>(
+                    const Color.fromARGB(255, 255, 193, 193),
+                  ),
+                ),
               ),
             ],
           ),
-          const SizedBox(height: 10,),
+          const SizedBox(height: 10),
           Column(
-            
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              TotalStatus(cant: asistencia.toInt(), type: 'Asistencia', color: Color(0xFF7e9758),),
-              TotalStatus(cant: vacaciones.toInt(), type: 'Vacaciones', color: kColorVacaciones,),
-              TotalStatus(cant: ausencias.toInt(), type: 'Ausencia', color: kColorFalta,),
+              TotalStatus(
+                cant: asistencia.toInt(),
+                type: 'Asistencia',
+                color: Color(0xFF7e9758),
+              ),
+              TotalStatus(
+                cant: vacaciones.toInt(),
+                type: 'Vacaciones',
+                color: kColorVacaciones,
+              ),
+              TotalStatus(
+                cant: ausencias.toInt(),
+                type: 'Ausencia',
+                color: kColorFalta,
+              ),
             ],
-          )
+          ),
         ],
       ),
     );
@@ -103,38 +127,50 @@ class TotalStatus extends StatelessWidget {
   final String type;
   final Color color;
   const TotalStatus({
-    super.key, required this.cant, required this.type, required this.color,
+    super.key,
+    required this.cant,
+    required this.type,
+    required this.color,
   });
 
   @override
   Widget build(BuildContext context) {
-    final textJt12bold400Secondary = Theme.of(context).textTheme.textJt12bold400Secondary;
-    final textJt13bold400Secondary = Theme.of(context).textTheme.textJt13bold400Secondary;
+    final textJt12bold400Secondary = Theme.of(
+      context,
+    ).textTheme.textJt12bold400Secondary;
+    final textJt13bold400Secondary = Theme.of(
+      context,
+    ).textTheme.textJt13bold400Secondary;
 
     final fontSizedGrow = getResponsiveScaleFactor(context);
     return Container(
       padding: const EdgeInsets.only(left: 10),
       child: Row(
-        
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-      
           Container(
-            width: 15*fontSizedGrow,
-            height: 15*fontSizedGrow,
+            width: 15 * fontSizedGrow,
+            height: 15 * fontSizedGrow,
             decoration: BoxDecoration(
               shape: BoxShape.circle,
-              border: BoxBorder.all(
-                color: color,
-                width: 2,
-      
-              )
+              border: BoxBorder.all(color: color, width: 2),
             ),
           ),
-          const SizedBox(width: 5,),
-          Text('$cant', style: textJt12bold400Secondary.copyWith(color: color,fontSize: 12*fontSizedGrow),),
-          const SizedBox(width: 5,),
-          Text(type,style: textJt13bold400Secondary.copyWith(fontSize: 13*fontSizedGrow))
+          const SizedBox(width: 5),
+          Text(
+            '$cant',
+            style: textJt12bold400Secondary.copyWith(
+              color: color,
+              fontSize: 12 * fontSizedGrow,
+            ),
+          ),
+          const SizedBox(width: 5),
+          Text(
+            type,
+            style: textJt13bold400Secondary.copyWith(
+              fontSize: 13 * fontSizedGrow,
+            ),
+          ),
         ],
       ),
     );
